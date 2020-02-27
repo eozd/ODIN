2458ba7b60ae3e6d96794d519e3fb3c93e0a7b37
Eshref Yozdemir <eshrefyoz@gmail.com>
2020-02-27 14:04:15 +0100

    Add setup.py
    So that the library can simply be added to conda
    environment.yml to automate the installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="odin",
+    version="0.0.1",
+    author="Gabriele Abbati",
+    author_email="ao@ot.co",
+    description="ODIN",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy', 'scipy', 'tensorflow'],
+    classifiers=(
+        "Programming Language :: Python :: 3"),)
